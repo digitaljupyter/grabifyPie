f0851ab183a3ba2becefb49cf6b126e4caf72d9c
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-10-03 21:09:22 +0000

    Use new path

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # Pyinstaller command to build the pie.py to output-bins
-pyinstaller --distpath=output-bins pie.py
+pyinstaller --distpath=output-bins ip/pie.py
 pyinstaller --distpath=output-bins roblox/roblox-scraper.py
 
 Remove-Item pie.spec
